0aafa33eb3696cf8beb6e0c1da4c40c08ae6542f
Xavier Palomo Teruel <44377663+xpalomoteruel@users.noreply.github.com>
2026-09-19 18:47:05 +0200

    Refine market outlook: expectations and scenarios

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fire-mobile-d53428d18df8d839';
+const CACHE='fire-mobile-9c8aaf9ec8d1187e';
 const FILES=["./icon-192.png","./icon-512.png","./index.html","./manifest.webmanifest","./snapshot.enc.json","./assets/index-BjT0Bruv.js","./assets/index-CKpCoQTl.css","./assets/manifest-B59zWbUn.webmanifest"];
 const SCOPE=new URL(self.registration.scope);
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
